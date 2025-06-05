154b63f3cc3b23241289c4635c173bfe2870813e
MarcosWillianAP1K <mwss@outlook.com>
2025-06-05 14:35:38 -0300

    Gerador de entradas pronto

--- Arquivos_entrada/Gerador_de_numeros/Gerardor.c
+++ Arquivos_entrada/Gerador_de_numeros/Gerardor.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define QUANT_NUMEROS 100
+#define QUANT_NUMEROS 10000
 
-#define Arquivo_cresente "../../numeros.txt"
+#define Arquivo_cresente "../../numeros_cresente.txt"
 #define Arquivo_decrescente "../../numeros_decrescente.txt"
 #define Arquivo_aleatorio "../../numeros_aleatorios.txt"
 
@@ -63,6 +63,8 @@ void gerarNumerosAleatorios()
         return;
     }
 
+    int vetor[QUANT_NUMEROS] = {0}; // Vetor para verificar números já existentes
+
     srand(time(NULL)); // Semente para números aleatórios
 
     for (int i = 0; i < QUANT_NUMEROS; i++)
@@ -70,7 +72,7 @@ void gerarNumerosAleatorios()
 
         int numero = rand() % QUANT_NUMEROS + 1; // Gera número entre 1 e QUANT_NUMEROS
         
-        while (verificar_se_numero_ja_existe(arquivo, numero))
+        while (verificar_se_numero_ja_existe(vetor, numero))
         {
             numero = rand() % QUANT_NUMEROS + 1; // Gera novo número se já existir
         }
@@ -81,9 +83,57 @@ void gerarNumerosAleatorios()
     fclose(arquivo);
 }
 
+void verificar_arquivos_foram_gerados()
+{
+    FILE *arquivoCrescente = fopen(Arquivo_cresente, "r");
+    FILE *arquivoDecrescente = fopen(Arquivo_decrescente, "r");
+    FILE *arquivoAleatorio = fopen(Arquivo_aleatorio, "r");
+
+    if (arquivoCrescente == NULL)
+    {
+        printf("Erro ao abrir os arquivos gerados.\n\n");
+    }
+
+    if (arquivoDecrescente == NULL)
+    {
+        printf("Erro ao abrir os arquivos gerados.\n\n");
+    }
+
+    if (arquivoAleatorio == NULL)
+    {
+        printf("Erro ao abrir os arquivos gerados.\n\n");
+    }
+
+    fclose(arquivoCrescente);
+    fclose(arquivoDecrescente);
+    fclose(arquivoAleatorio);
+
+    if (arquivoCrescente != NULL && arquivoDecrescente != NULL && arquivoAleatorio != NULL)
+    {
+        printf("Todos os arquivos foram gerados com sucesso!\n\n");
+    }
+    else
+    {
+        printf("Alguns arquivos nao foram gerados corretamente.\n\n");
+    }
+}
+
 int main()
 {
 
+    printf("Gerando numeros em ordem crescente...\n");
+    gerarNumerosCrescente();
+    printf("Sucesso! Numeros em ordem crescente gerados.\n\n");
+
+    printf("Gerando numeros em ordem decrescente...\n");
+    gerarNumerosDecrescente();
+    printf("Sucesso! Numeros em ordem decrescente gerados.\n\n");
+
+    printf("Gerando numeros aleatorios...\n");
+    gerarNumerosAleatorios();
+    printf("Sucesso! Numeros aleatorios gerados.\n\n");
+
+    verificar_arquivos_foram_gerados();
 
     return 0;
 }
